7d1cb40d0b1ee0163b61cdfc2348c8850812ecb4
marcelo0410 <cs95401156@gmail.com>
2022-04-06 00:14:03 +0800

    Test animation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Homepage from './screens/Home/index';
 import CurrentEnv from './screens/currentenv/index'
 import Calculation from './screens/calculation/index'
+import Test from './screens/Test/Ani'
 import './App.css';
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<Homepage/>}/>
           <Route path="/curenv" element={<CurrentEnv/>}/>
           <Route path="/calculator" element={<Calculation/>}/>
+          <Route path="/test" element={<Test scrollHeight={4000} width={1158} height={770} numFrames={63}/>}/>
           <Route path="/" element={<Navigate replace to="/home"/>} />
         </Routes>
         <Footer/>
