919b6199f42ec1043e895387d6c692c681d55f32
marcelo0410 <cs95401156@gmail.com>
2022-04-09 23:09:18 +0800

    Calculator page

--- src/screens/calculation/index.js
+++ src/screens/calculation/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Calculator from './components/Calculator'
+// import Calculator from './components/CalculatorOri'
 export default function index() {
   return (
     <div>
-        <Calculator/>
+        {/* <Calculator/> */}
     </div>
   )
 }
