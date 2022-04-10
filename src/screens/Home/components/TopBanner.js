ae0b8c64b7840ff7bb4d07d06724e267297be2b5
marcelo0410 <cs95401156@gmail.com>
2022-04-11 07:16:15 +0800

    homepage direct

--- src/screens/Home/components/TopBanner.js
+++ src/screens/Home/components/TopBanner.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {useLocation, useNavigate} from 'react-router-dom';
 import './style.css'
 import homeCarImage from '../../../assets/homepage/homepage-car.png'
 
 export default function TopBanner() {
+  const navigate = useNavigate()
+  const handleClick = () =>{
+    navigate('/curenv')
+  }
+
   return (
     <div className='banner'>
       <div style={{color:"white", fontWeight:"bold", fontSize:"70px", marginTop:"140px"}}>Change it now!</div>
       <div style={{color:"white", fontSize:"30px", marginBottom:"30px"}}>Why should I purchase an EV instead of a fossil fueled one?</div>
-      <button className="button-know" type="button">Learn more</button>
+      <button className="button-know" type="button" onClick={handleClick}>Learn more</button>
     </div>
   )
 }
