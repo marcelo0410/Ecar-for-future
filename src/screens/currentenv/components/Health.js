acb9e35ce3dcb3ba52b1368a5457608c8e4f9492
marcelo0410 <cs95401156@gmail.com>
2022-04-11 23:47:51 +0800

    CurrentEnv health component

--- src/screens/currentenv/components/Health.js
+++ src/screens/currentenv/components/Health.js
@@ -2,6 +2,7 @@ import React from 'react'
 import healthHeart from '../../../assets/currentenv/health-human-heart.png'
 import healthBreath from '../../../assets/currentenv/health-breath.png'
 import healthPharynx from '../../../assets/currentenv/health-pharynx.png'
+import './style.css'
 export default function Health() {
   return (
     <div>
@@ -17,9 +18,9 @@ export default function Health() {
                     <img src={healthPharynx} style={{height:"170px", width:"170px"}}></img>
                 </div>
             </div>
-            <div className='health-text-area'>Healthy impact due to the pollutants:
+            <div className='health-text-area'>
+                <div>Fuel vehicle exhaust pollution significantly affects <br/>people's health and increases the risk of diseases:</div>
                 <ul className='text-start'>
-                    <li>Increasing people’s risk of developing</li>
                     <li>Cardiovascular disease</li>
                     <li>Hespiratory infections </li>
                     <li>Chronic obstructive pulmonary disease (COPD) </li>
