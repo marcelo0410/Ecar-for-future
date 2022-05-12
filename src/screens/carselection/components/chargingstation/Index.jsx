e2aa498a90c80989a8ed7e0c563bbefa9fb2367b
marcelo0410 <cs95401156@gmail.com>
2022-05-13 04:42:09 +1000

    charging map change location

--- src/screens/carselection/components/chargingstation/Index.jsx
+++ src/screens/carselection/components/chargingstation/Index.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 // import * as chargingpointData from "../../../../assets/data/cpm_mod.json";
 import * as chargingpointData from "./cpm_mod.json";
 import style from './style.module.css'
@@ -17,11 +17,18 @@ export default function Index(props) {
     }
   }, [props])
   
+  const ChangeView = ({ center, zoom }) =>{
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+
+
   return (
     <div>
       {console.log(props.coordinate)}
     <MapContainer center={props.coordinate} zoom={13} scrollWheelZoom={true} className={style.leaflet_container}>
-      
+      <ChangeView center={props.coordinate} zoom={14}/>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
